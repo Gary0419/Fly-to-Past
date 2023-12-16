0e6b456a2f9a59a3c625e1ed67172797064b9f6f
garysblog <gary90419@gmail.com>
2023-12-17 00:29:55 +0800

    second version

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import os
 
@@ -62,37 +63,87 @@ def flightIns():
 
 @app.route('/flight/insert/submit', methods=['POST'])
 def flightInsSubmit():
-    start_date = request.form.get('start_date')
-    end_date = request.form.get('end_date')
     departure = request.form.get('departure')
     destination = request.form.get('destination')
+    sql_cmd = f"""
+                Select route_id
+                From route
+                Where depart_airport = {departure}
+                And arrive_airport = {destination};
+                """
+    route = db.session.execute(text(sql_cmd)).fetchall()
+
+    sql_cmd = "Select Max(flight_id) From flight;"
+    max_id = db.session.execute(text(sql_cmd)).fetchall()
+
+    id = max_id[0][0] + 1
+    depart_time = request.form.get('depart_time')
+    arrive_time = request.form.get('arrive_time')
+    airplane = request.form.get('airplane')
+    route_id = route[0][0]
+    max_luggage_capacity = request.form.get('max_luggage_capacity')
+    max_sell_amount = request.form.get('max_sell_amount')
 
     sql_cmd = f"""
-                Select f.*, r.depart_airport, r.arrive_airport, s.ticket_sold
-                From (Select *
-                        From flight
-                        Where DATE(depart_time) Between '{start_date}' And
-                        '{end_date}') As f
-                Join route As r on f.route_id = r.route_id
-                Left Join (Select flight_id , Count(*) As ticket_sold
-                            From ticket
-                            Group by flight_id) As s on f.flight_id = s.flight_id
-                Where r.depart_airport = {departure} And r.arrive_airport = {destination}
-                Order By f.depart_time;
+                INSERT INTO flight (
+                    flight_id,
+                    plane_id,
+                    route_id,
+                    depart_time,
+                    arrive_time,
+                    max_luggage_capacity,
+                    max_sell_amount
+                ) VALUES (
+                    {id},
+                    {airplane},
+                    {route_id},
+                    '{depart_time}',
+                    '{arrive_time}',
+                    {max_luggage_capacity},
+                    {max_sell_amount}
+                );
+                """
+ 
+    db.session.execute(text(sql_cmd))
+    db.session.commit()
+    return 'Success'
+
+@app.route("/flight/delete")
+def flightDel():
+    return render_template('flightdelete.html')
+
+@app.route("/flight/delete/submit", methods=['POST'])
+def flightDelSubmit():
+    id = request.form.get('flight_id')
+
+    sql_cmd = f"""
+                Delete From flight
+                Where flight_id = {id};
+                """
+ 
+    db.session.execute(text(sql_cmd))
+    db.session.commit()
+    return "Success"
+
+@app.route("/flight/crew")
+def flightCrew():
+    return render_template('flightcrew.html')
+
+@app.route("/flight/crew/submit", methods=['POST'])
+def flightCrewSubmit():
+    id = request.form.get('flight_id')
+
+    sql_cmd = f"""
+                Select *
+                From execute
+                Where flight_id = {id}
+                Order by employee_title;
                 """
  
     query_data = db.session.execute(text(sql_cmd))
     keys = query_data.keys()
     query_data = query_data.fetchall()
-    return 'Success'
-
-@app.route("/flight/delete")
-def flightDel():
-    return "Hello, World!"
-
-@app.route("/flight/delete/submit")
-def flightDelSubmit():
-    return "Hello, World!"
+    return render_template('flightcrewsubmit.html', execute = query_data, keys = keys)
 
 @app.route("/employee/position")
 def empPosition():
@@ -141,14 +192,62 @@ def empIdSubmit():
 
 @app.route("/partschedule")
 def partSchedule():
-    return "Hello, World!"
+    start_date = datetime.now().date()
+    end_date = start_date + timedelta(days=6)
+
+    sql_cmd = f"""
+        SELECT pt.parts_id, pt.plane_id, mt.last_mt + pt.maintain_frequency AS mt_needed
+        FROM (
+            SELECT parts_id, MAX(maintain_date) AS last_mt
+            FROM maintain
+            GROUP BY parts_id
+        ) AS mt
+        JOIN parts AS pt ON mt.parts_id = pt.parts_id
+        WHERE mt.last_mt + pt.maintain_frequency BETWEEN '{start_date}' AND '{end_date}'
+        ORDER BY mt_needed, pt.plane_id;
+    """
+    query_data = db.session.execute(text(sql_cmd))
+    keys = query_data.keys()
+    query_data = query_data.fetchall()
+    return render_template('test.html', data = query_data, keys = keys)
 
 @app.route("/maintainrecord")
 def maintainRecord():
-    return "Hello, World!"
+    return render_template('maintainrecord.html')
 
 @app.route("/maintainrecord/submit")
 def maintainRecordSubmit():
+    mt_date = request.form.get('mt_date')
+    worker_id = request.form.get('worker_id')
+    parts_id = request.form.get('parts_id')
+    mt_cost = request.form.get('mt_cost')
+
+    sql_cmd = f"""
+                Insert Into maintain (parts_id , employee_id, maintain_date , maintain_fee)
+                Values ({parts_id}, {worker_id}, '{mt_date}', {mt_cost});
+                """
+ 
+    db.session.execute(text(sql_cmd))
+    db.session.commit()
+    return "Success"
+
+@app.route("/ticket/update")
+def ticUpdate():
+    return render_template('ticketupdate.html')
+
+@app.route('/ticket/update/submit', methods=['POST'])
+def ticUpdateSubmit():
+    id = request.form.get('ticket_id')
+    status = request.form.get('status')
+
+    sql_cmd = f"""
+                Update ticket
+                Set status = '{status}'
+                Where ticket_id = {id};
+                """
+ 
+    db.session.execute(text(sql_cmd))
+    db.session.commit()
     return "Success"
 
 if __name__ == '__main__':
